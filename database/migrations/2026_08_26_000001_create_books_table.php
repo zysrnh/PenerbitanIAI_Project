b444af0af9c2afd42802f27e222d02898cabbdca
Naooo <naooolaf@gmail.com>
2026-08-26 23:13:10 +0700

    fix: restore proper namespace backslashes in create_books_table migration

--- database/migrations/2026_08_26_000001_create_books_table.php
+++ database/migrations/2026_08_26_000001_create_books_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use IlluminateDatabaseMigrationsMigration;
-use IlluminateDatabaseSchemaBlueprint;
-use IlluminateSupportFacadesSchema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
